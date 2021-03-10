c8c561feaaadbd325507909cf4dafbb80d72aba5
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-03-09 18:55:05 -0800

    test script fix.

--- run-test.sh
+++ run-test.sh
@@ -3,6 +3,4 @@ MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE
 MY_DIR="$(dirname $MY_PATH)"
 cd $MY_DIR
 
-export TS_NODE_COMPILER_OPTIONS="{\"module\": \"commonjs\" }"
-
-mocha -r ./node_modules/ts-node/register 'test/**/*.ts'
+npm test
